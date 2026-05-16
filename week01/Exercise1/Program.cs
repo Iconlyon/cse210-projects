9b876908e5bd29993aea2b8aec37552af55e9d8b
Iconlyon <lyoniorngulum@gmail.com>
2026-05-16 23:16:59 +0100

    week 1 exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Let me ask for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        //Let me Ask for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //Let me Display result
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
