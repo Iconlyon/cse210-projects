13c447bb825ab784b69c82d299270e72b71087a7
Iconlyon <lyoniorngulum@gmail.com>
2026-06-06 19:43:15 +0100

    youtube and online ordering

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("28, Gana Street, Maitama", "Maitama", "Abuja", "Nigeria");
+        Customer customer1 = new Customer("Daniel Abughdyer ", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop Stand", "LS100", 25.99, 2));
+        order1.AddProduct(new Product("Wireless Mouse", "WM200", 15.50, 1));
+        order1.AddProduct(new Product("USB-C Cable", "UC300", 8.75, 3));
+
+        Address address2 = new Address("45 Unity Road", "Lagos", "Lagos State", "Nigeria");
+        Customer customer2 = new Customer("Mary Johnson", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Notebook", "NB400", 6.99, 4));
+        order2.AddProduct(new Product("Backpack", "BP500", 32.50, 1));
+        order2.AddProduct(new Product("Water Bottle", "WB600", 12.25, 2));
+
+        Console.WriteLine("ORDER 1");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order1.GetPackingLabel());
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.GetShippingLabel());
+
+        Console.WriteLine($"Total Price: ${order1.GetTotalCost():0.00}");
+        Console.WriteLine("----------------------------------------");
+
+        Console.WriteLine("ORDER 2");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order2.GetPackingLabel());
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.GetShippingLabel());
+
+        Console.WriteLine($"Total Price: ${order2.GetTotalCost():0.00}");
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("Introduction to C# Programming", "Daniel Abughdyer", 420);
+        video1.AddComment(new Comment("Daniel", "This helped me understand the basics of C#."));
+        video1.AddComment(new Comment("Mary", "The explanation was clear and easy to follow."));
+        video1.AddComment(new Comment("Samuel", "Great video for beginners."));
+
+        Video video2 = new Video("Understanding Classes and Objects", "Programming Basics", 600);
+        video2.AddComment(new Comment("Grace", "Classes and objects make more sense now."));
+        video2.AddComment(new Comment("Peter", "I liked the simple examples."));
+        video2.AddComment(new Comment("Sarah", "This helped me with my assignment."));
+
+        Video video3 = new Video("Abstraction in Object-Oriented Programming", "Computer Science Tutorials", 750);
+        video3.AddComment(new Comment("Michael", "This was a useful explanation of abstraction."));
+        video3.AddComment(new Comment("Joy", "I understand why abstraction is important now."));
+        video3.AddComment(new Comment("David", "The examples made the concept easier to understand."));
+
+        Video video4 = new Video("Encapsulation in C#", "Software Development", 540);
+        video4.AddComment(new Comment("Blessing", "Encapsulation is much clearer to me now."));
+        video4.AddComment(new Comment("James", "Good explanation of private variables and methods."));
+        video4.AddComment(new Comment("Esther", "This was very helpful for learning OOP."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            video.DisplayVideoInfo();
+        }
     }
 }
