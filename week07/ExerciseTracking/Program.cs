c83a22e72dd8497bdfc823a2d9fa94c7b4e4924d
Iconlyon <lyoniorngulum@gmail.com>
2026-06-18 15:15:38 +0100

    week07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Jan 2025", 10, 6.8));
+        activities.Add(new Cycling("04 Jan 2025", 35, 10.0));
+        activities.Add(new Swimming("05 Jan 2025", 20, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
